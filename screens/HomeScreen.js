71b500d8afb0ebd3ba3e4855b1df36e24a9b3ced
brentaharris <brentaustinharris@gmail.com>
2022-06-24 16:23:29 -0400

    corrected image src error

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,13 @@
 import { ScrollView, Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
 import { useSelector } from 'react-redux';
-import { baseURL } from '../shared/baseUrl';
+import { baseUrl } from '../shared/baseUrl';
 
 const FeaturedItem = ({item}) => {
     if (item) {
         return (
             <Card containerStyle={{padding: 0}}>
-                <Card.Image source={{ uri: baseURL + item.image }} >
+                <Card.Image source={{ uri: baseUrl + item.image }} >
                     <View style={{justifyContent: 'center', flex: 1}}>
                         <Text style={{color: 'white', textAlign: 'center', fontSize: 20}}>
                             {item.name}
