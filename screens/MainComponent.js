ac05b5c9f18f89cf142fbd0d3888634357c3b931
brentaharris <brentaustinharris@gmail.com>
2022-06-25 19:15:42 -0400

    added favorites screen

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -9,6 +9,7 @@ import DirectoryScreen from './DirectoryScreen';
 import AboutScreen from './AboutScreen';
 import ContactScreen from './ContactScreen';
 import ReservationScreen from './ReservationScreen';
+import FavoritesScreen from './FavoritesScreen';
 import logo from '../assets/images/logo.png';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
@@ -151,6 +152,30 @@ const ReservationNavigator = () => {
     )
 }
 
+const FavoritesNavigator = () => {
+    const Stack = createStackNavigator();
+    return(
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen 
+                name='Favorites'
+                component={FavoritesScreen}
+                options={({ navigation }) => ({ 
+                    title: 'Favorite Campsites',
+                    headerLeft: () => (
+                        <Icon
+                            name='heart'
+                            type='font-awesome'
+                            iconStyle={styles.stackIcon}
+                            onPress={() => navigation.toggleDrawer()}
+                        />
+                    )
+                })}
+            />
+
+        </Stack.Navigator>
+    )
+}
+
 const CustomDrawerContent = (props) => (
     <DrawerContentScrollView {...props}>
         <View style={styles.drawerHeader}>
@@ -233,6 +258,24 @@ const Main = () => {
                         )
                      }}
                 />
+
+                <Drawer.Screen 
+                    name='Favorites'
+                    component={FavoritesNavigator}
+                    options={{ 
+                        title: 'My Favorites',
+                        drawerIcon: ({ color }) => (
+                            <Icon 
+                                name='heart'
+                                type='font-awesome'
+                                size={24}
+                                iconStyle={{ width: 24 }}
+                                color={color}
+                            />
+                        )
+                     }}
+                />
+
                 <Drawer.Screen
                     name='About'
                     component={AboutNavigator}
