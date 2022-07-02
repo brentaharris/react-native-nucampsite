5fa9addd97d5123f5a1a2ae8b8867757f11fcfcc
brentaharris <brentaustinharris@gmail.com>
2022-07-02 12:47:38 -0400

    updated gestures

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import { NavigationContainer } from '@react-navigation/native';
 import Main from './screens/MainComponent';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import Loading from './components/LoadingComponent';
 
 // json-server -H 0.0.0.0 --watch db.json -p 3001 -d 2000
 
 export default function App() {
     return (
         <Provider store={store}>
-            <NavigationContainer>
-                <Main />
-            </NavigationContainer> 
+            <PersistGate loading={<Loading />} persistor={persistor}>
+                <NavigationContainer>
+                    <Main />
+                </NavigationContainer> 
+            </PersistGate>
         </Provider>   
     )
 };
