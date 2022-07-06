0710c087d64dc66677e259ade3c09770f8390232
brentaharris <brentaustinharris@gmail.com>
2022-07-05 20:26:10 -0400

    tab navigation, updated login screen

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -22,6 +22,7 @@ import ContactScreen from './ContactScreen';
 import ReservationScreen from './ReservationScreen';
 import FavoritesScreen from './FavoritesScreen';
 import LoginScreen from './LoginScreen';
+import { getFocusedRouteNameFromRoute } from '@react-navigation/core';
 
 
 const Drawer = createDrawerNavigator();
@@ -152,15 +153,18 @@ const LoginNavigator = () => {
             <Stack.Screen
                 name='Login'
                 component={LoginScreen}
-                options={({ navigation }) => ({
+                options={({ navigation, route }) => ({
+                    headerTitle: getFocusedRouteNameFromRoute(route),
                     headerLeft: () => (
-                        <Icon
-                            name='sign-in'
+                        <Icon 
+                            name={
+                                getFocusedRouteNameFromRoute(route) === 'Register' ? 'user-plus' : 'sign-in'
+                            }
                             type='font-awesome'
                             iconStyle={styles.stackIcon}
                             onPress={() => navigation.toggleDrawer()}
                         />
-                    )
+                    ),
                 })}
             />
         </Stack.Navigator>
